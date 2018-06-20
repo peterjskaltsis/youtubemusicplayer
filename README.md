3311e519da5fa3b35378b5a929800d8b51441c6a
Peter S <pjs300300@gmail.com>
2018-06-20 16:36:18 +1000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# youtubemusicplayer
-A simple Youtube Music player for you to run as an app, similar to spotify, and enjoy seamlessly.
+# Desktop Youtube Music Player
+
+**Looking for a way to listen to Youtube Music on your Computer?**
+
+This is a simple Youtube Music player for you to run as an app, similar to spotify, and enjoy seamlessly.
+
+## To Use
+
+**For Mac**
+Download the installer below and run the app, sign in to your Youtube account (this is purely an interaction with Google) and enjoy the tunes!
+
+## License
+
+MIT License
---
+++ main.js
@@ -0,0 +1,44 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1200, height: 800, titleBarStyle: 'hidden-inset', backgroundColor: '#000000'})
+
+  // and load youtube music and adjust styling for top bar.
+  mainWindow.loadURL('https://music.youtube.com')
+  mainWindow.webContents.on('did-finish-load', function() {
+    mainWindow.webContents.insertCSS('ytmusic-app:before {width: 100%;height: 36px;top: 0;display: block;content: "";background: #1b1b1b;-webkit-app-region: drag;}') 
+ });
+
+  mainWindow.once('ready-to-show', function (){
+         newWindow.show();
+     });
+
+  //When the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+//Call whe electron has finished & has initialised
+app.on('ready', createWindow)
+
+//If all windows are closed, quit.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
