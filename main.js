0cc9b559af2dfc53c798fdf11350c9bf32c4fd41
Peter S <pjs300300@gmail.com>
2018-06-20 21:33:48 +1000

    finalised 1

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path')
+var path = require('path')
 const url = require('url')
 
 let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1200, height: 800, titleBarStyle: 'hidden-inset', backgroundColor: '#000000'})
+  mainWindow = new BrowserWindow({width: 1200, height: 800, titleBarStyle: 'hidden-inset', backgroundColor: '#000000', title:'Youtube Music Player', icon: path.join(__dirname + '/icon.icns')})
 
   // and load youtube music and adjust styling for top bar.
   mainWindow.loadURL('https://music.youtube.com')
   mainWindow.webContents.on('did-finish-load', function() {
     mainWindow.webContents.insertCSS('ytmusic-app:before {width: 100%;height: 36px;top: 0;display: block;content: "";background: #1b1b1b;-webkit-app-region: drag;}') 
  });
+ mainWindow.setTitle('Youtube Music Player')
 
   mainWindow.once('ready-to-show', function (){
          newWindow.show();
